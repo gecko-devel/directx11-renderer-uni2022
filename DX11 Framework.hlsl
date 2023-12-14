61bdea42ecd4f3333b474466f3a2e31bf28c5710
Joshua Conway <josh@conway.onl>
2023-12-14 16:42:24 +0000

    Initial commit

---
+++ DX11 Framework.hlsl
@@ -0,0 +1,46 @@
+//--------------------------------------------------------------------------------------
+// File: DX11 Framework.hlsl
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+//--------------------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------------------
+// Constant Buffer Variables
+//--------------------------------------------------------------------------------------
+cbuffer ConstantBuffer : register( b0 )
+{
+	matrix World;
+	matrix View;
+	matrix Projection;
+}
+
+//--------------------------------------------------------------------------------------
+struct VS_OUTPUT
+{
+    float4 Pos : SV_POSITION;
+    float4 Color : COLOR0;
+};
+
+//--------------------------------------------------------------------------------------
+// Vertex Shader
+//--------------------------------------------------------------------------------------
+VS_OUTPUT VS( float3 Pos : POSITION, float4 Color : COLOR )
+{
+    float4 pos4 = float4(Pos, 1.0f);
+    
+    VS_OUTPUT output = (VS_OUTPUT)0;
+    output.Pos = mul( pos4, World);
+    output.Pos = mul( output.Pos, View );
+    output.Pos = mul( output.Pos, Projection );
+    output.Color = Color;
+    return output;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Pixel Shader
+//--------------------------------------------------------------------------------------
+float4 PS( VS_OUTPUT input ) : SV_Target
+{
+    return input.Color;
+}
